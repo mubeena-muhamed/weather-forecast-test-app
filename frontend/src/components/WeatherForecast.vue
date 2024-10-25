789dea2603029344bef785c2d2fbdffaf646fc6a
mubeena <mubeena@legalinc.com>
2024-10-25 16:26:33 +0530

    WeatherForecast UI updates

--- frontend/src/components/WeatherForecast.vue
+++ frontend/src/components/WeatherForecast.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="hello">
-    <h3>Weather forecast for city</h3>
-
-    TODO
-    
+  <div data-cy="weather-forecast" class="border border-gray-300 rounded-lg p-5 max-w-md mx-auto my-5 shadow-md bg-white">
+    <h3 class="mb-4 text-xl text-center">Weather Forecast for City</h3>
+    <div v-if="errorMessage" class="text-red-600 text-center">{{ errorMessage }}</div>
+    <div v-else-if="weatherForecast">
+      <div class="text-center">
+        <p class="font-bold text-lg">Temperature: {{ weatherForecast.temperature }}°C</p>
+        <p class="text-gray-600">Conditions: {{ weatherCondition }}</p>
+        <p class="text-gray-600">Wind Speed: {{ weatherForecast.windspeed }} km/h</p>
+        <p class="text-gray-600">Wind Direction: {{ windDirection }}°</p>
+      </div>
+    </div>
+    <div v-else>
+      <p class="text-center">Loading City Weather Forecast...</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts" src="./weather-forecast.component.ts"></script>
-
-<style>
-
-</style>
--- frontend/src/components/weather-forecast.component.ts
+++ frontend/src/components/weather-forecast.component.ts
@@ -1,6 +1,8 @@
 import WeatherService from '@/services/weather-service.service';
 import { Options, Vue } from 'vue-class-component';
-import { Inject } from 'vue-property-decorator';
+import { Inject, Watch } from 'vue-property-decorator';
+
+import { WEATHER_CODES, WIND_DIRECTION_DESCRIPTIONS } from '@/constants/weatherConstants';
 
 @Options({
   props: {
@@ -11,14 +13,67 @@ export default class WeatherForecast extends Vue {
   @Inject('weatherService')
   public weatherService!: WeatherService;
 
-   
+  @Inject('citySelectionService')
+  citySelectionService!: { selectedPlace: { lat: number; lng: number } | null };
+  
+  weatherForecast: any = null;
+  errorMessage: string | null = null;
+
+
   mounted() {
-    // TODO - use the latitude and longitude from the search city component
-    // TODO - display the weather forecast in the template
-    // TODO - Error handling, if the API call fails we should display an error message
-    this.weatherService.getWeatherForecast(52.52, 13.419998).then((res) => {
-      console.log(res);
-    });
+    this.fetchWeatherForecast(null)
+  }
+
+  @Watch('citySelectionService.selectedPlace', { immediate: true, deep: true })
+  onSelectedPlaceChanged(newPlace: { lat: number; lng: number } | null) {
+    if (newPlace) {
+      this.fetchWeatherForecast(newPlace);
+    }
+  }
+
+  async fetchWeatherForecast(selectedPlace: { lat: number; lng: number } | null) {
+    this.errorMessage = '';
+    if (selectedPlace) {
+      const { lat, lng } = selectedPlace;
+      try {
+        const result = await this.weatherService.getWeatherForecast(lat, lng);
+        this.weatherForecast = result.current_weather;
+        console.log('Weather data:', this.weatherForecast.temperature);
+      } catch (error) {
+        this.errorMessage = 'Failed to fetch weather data. Please try again.';
+        console.error('Error fetching weather data:', error);
+      }
+    } else {
+      this.errorMessage = 'Please select a location first.';
+    }
+  }
+
+  get weatherCondition() {
+    return WEATHER_CODES[this.weatherForecast?.weathercode] || 'Unknown weather condition';
+  }
+
+  get windDirection() {
+    const windDirection = this.weatherForecast?.winddirection;
+
+    if (windDirection === 0 || windDirection === 360) {
+        return WIND_DIRECTION_DESCRIPTIONS['0'];
+    } else if (windDirection > 0 && windDirection < 90) {
+        return WIND_DIRECTION_DESCRIPTIONS['1-89'];
+    } else if (windDirection === 90) {
+        return WIND_DIRECTION_DESCRIPTIONS['90'];
+    } else if (windDirection > 90 && windDirection < 180) {
+        return WIND_DIRECTION_DESCRIPTIONS['91-179'];
+    } else if (windDirection === 180) {
+        return WIND_DIRECTION_DESCRIPTIONS['180'];
+    } else if (windDirection > 180 && windDirection < 270) {
+        return WIND_DIRECTION_DESCRIPTIONS['181-269'];
+    } else if (windDirection === 270) {
+        return WIND_DIRECTION_DESCRIPTIONS['270'];
+    } else if (windDirection > 270 && windDirection < 360) {
+        return WIND_DIRECTION_DESCRIPTIONS['271-359'];
+    } else {
+        return 'Unknown direction';
+    }
   }
 
 }

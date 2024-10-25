60d1b05d18243d9a49b8246b6e5e1b2886bdb948
mubeena <mubeena@legalinc.com>
2024-10-25 19:11:56 +0530

    UI updates

--- frontend/src/components/WeatherForecast.vue
+++ frontend/src/components/WeatherForecast.vue
@@ -1,17 +1,45 @@
 <template>
   <div data-cy="weather-forecast" class="border border-gray-300 rounded-lg p-5 max-w-md mx-auto my-5 shadow-md bg-white">
-    <h3 class="mb-4 text-xl text-center">Weather Forecast for City</h3>
-    <div v-if="errorMessage" class="text-red-600 text-center">{{ errorMessage }}</div>
-    <div v-else-if="weatherForecast">
-      <div class="text-center">
-        <p class="font-bold text-lg">Temperature: {{ weatherForecast.temperature }}°C</p>
-        <p class="text-gray-600">Conditions: {{ weatherCondition }}</p>
-        <p class="text-gray-600">Wind Speed: {{ weatherForecast.windspeed }} km/h</p>
-        <p class="text-gray-600">Wind Direction: {{ windDirection }}°</p>
+
+    <div data-cy="weather-forecast" class="max-w-full mx-auto px-4">
+      <div class="flex justify-center">
+        <div class="w-full max-w-md"> 
+          <div class="bg-white rounded-3xl p-4 shadow-lg">   
+
+
+
+            <h3 class="mb-4 text-2xl text-center">Weather Forecast for City</h3>
+            <div v-if="errorMessage" class="text-red-600 text-center">{{ errorMessage }}</div>
+            <div v-else-if="weatherForecast">
+              <div class="text-center">
+                <div class="flex flex-col items-center mt-5 mb-3">
+                  <h1 class="mb-0 text-5xl text-gray-800 font-bold" id="heading">{{ weatherForecast.temperature }}° C</h1>
+                  <span class="text-gray-500 text-md">{{ weatherCondition }}</span>
+                </div>
+
+                <div class="flex">
+                  <div class="flex-grow">
+                    <p class="my-1 flex items-center">
+                      <img src="@/assets/images/wind.png" height="17px" alt="Wind Speed Icon" class="mr-1">
+                      <span>{{ weatherForecast.windspeed }} km/h</span>
+                      <span>{{ windDirection }}°</span> 
+                    </p>
+
+                  
+                  </div>
+                  <p class="my-1 flex items-center"> 
+                      <img src="@/assets/images/cloud.png" height="17px" alt="Cloud Icon" class="mr-1">
+                    </p>
+              
+                </div>
+              </div>
+            </div>
+            <div v-else>
+              <p class="text-center">Loading City Weather Forecast...</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div v-else>
-      <p class="text-center">Loading City Weather Forecast...</p>
     </div>
   </div>
 </template>

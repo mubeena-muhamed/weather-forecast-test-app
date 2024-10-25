f5749e5898107a9b3ea4a25fa1b0bdad3f86ba92
mubeena <mubeena@legalinc.com>
2024-10-25 16:21:36 +0530

    Docker configurations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = process.env.VUE_APP_BACKEND_PORT || 4000;
 
 app.use(cors());
 app.use(express.json() as express.RequestHandler);

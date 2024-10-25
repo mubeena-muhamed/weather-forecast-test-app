1b7f6bbcec12d2627afd76a63a1826d2dd3e642c
mubeena <mubeena@legalinc.com>
2024-10-25 16:41:57 +0530

    port updates

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_FRONTEND_BASE_URL || 'http://localhost:8081',
+    baseUrl: process.env.CYPRESS_FRONTEND_BASE_URL || 'http://localhost:8080',
     specPattern: 'cypress/e2e/**/*.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
